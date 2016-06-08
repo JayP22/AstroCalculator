5dcf22560bc7d0b84946f05fbdd41ff2bfe9a9d3
JayP22 <Josh_Pre96@hotmail.com>
2016-06-08 12:49:52 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+/* Used this certain code to make a math formula to multiply the weight by the weight that you put into the text box */
 function calculateWeight() {
     var myWeight = document.getElementById("myWeight").value;
     var myPlanet = document.getElementById("myPlanet").value;
 
     var myNewWeight = myWeight * myPlanet;
     var myPlanetSelection = document.getElementById("myPlanet")[document.getElementById("myPlanet").selectedIndex].text;
-
+/* Used ' if ' to make it where when the user chooses either moon or sun it adds ' the ' to the sentene to make it proper */
     if (myPlanetSelection == 'Sun' || myPlanetSelection == 'Moon') {
         document.getElementById("answer").innerHTML = "If you were on the " + myPlanetSelection + " you would weigh " + myNewWeight + " lbs! ";
     } else {
